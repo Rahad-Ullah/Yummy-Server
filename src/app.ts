73aeb7e83aed00d71b9297204ec2247e2fc5aa79
Rahad-Ullah <myemail.abc20@gmail.com>
2024-10-15 05:54:26 +0600

    The router is setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import router from './app/routes'
+import cookieParser from 'cookie-parser'
+
 const app: Application = express()
 
 // parsers
 app.use(express.json())
 app.use(cors())
+app.use(cookieParser())
+
+app.use('/api/v1', router)
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello world. Server is running...')
+  res.send('Yummy Server is running...')
 })
 
 export default app
